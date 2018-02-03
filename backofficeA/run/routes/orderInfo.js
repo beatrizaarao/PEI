ea2187dc0cca2a9265d9f12f0106f8032da1862b
beatrizaarao <beatrizaarao@gmail.com>
2018-02-03 16:07:40 +0000

    Encomendas e imprimir direito Falta por bonito

---
+++ backofficeA/run/routes/orderInfo.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var router = express.Router();
+const TreeMap = require("treemap-js");
+
+/* GET home page. */
+router.get('/:orderID', function(req, res, next) {
+    var stepService = new TreeMap();
+    var serviceOption = new TreeMap();
+    var steps = [];
+    var services =  [];
+    var array = new Array();
+    var array2 = new Array();
+    if (req.cookies.deploy === undefined) {
+        res.redirect('/')
+    }
+    else if (req.cookies.online === undefined) {
+        res.redirect('/')
+    }
+    else {
+
+        var db = req.connection;
+        db.query("SELECT * FROM ORDER_STEP_SERVICE_OPTION WHERE ID_ORDER=?", req.params.orderID, function (error, result, client) {
+            var results = result;
+            var total = results.length;
+            var count = 0;
+            for(var i = 0; i < total; i++){
+                (function(foo) {
+                    array = [];
+                    var currentStep = results[foo].id_STEP;
+                    var currentServiceID = results[foo].id_SERVICE;
+                    var currentOptionID = results[foo].id_OPTION;
+                    db.query("SELECT NAME FROM STEP WHERE id_STEP=?", currentStep, function (error, result, client) {
+                        var stepName = result[0].NAME
+                        if(steps.includes(stepName)==false){
+                            steps.push(stepName);
+                        }
+                        db.query("SELECT NAME FROM SERVICE WHERE id_SERVICE=?", currentServiceID, function (error, result, client) {
+                            var serviceName = result[0].NAME
+                            services.push(serviceName);
+                            db.query("SELECT NAME FROM OPCAO WHERE id_OPTION=?", currentOptionID, function (error, result, client) {
+                                if(result != null){
+                                    var optionName = result[0].NAME
+                                    array2 = serviceOption.get(serviceName);
+
+                                    if(array2== undefined){
+                                        if(results[foo].opcao_valor==null){array2 = [optionName];}
+                                        else{array2 = [optionName + ": " + results[foo].opcao_valor];}
+
+                                        serviceOption.set(serviceName, array2);
+                                    }
+                                    else {
+                                        if(results[foo].opcao_valor==null){array2.push(optionName);}
+                                        else{array2.push(optionName + ": " + results[foo].opcao_valor);}
+
+                                    }
+                                }
+                                array = stepService.get(stepName);
+                                if (array == undefined) {
+                                    if(results[foo].SERVICE_valor==null){array = [serviceName];}
+                                    else{array = [serviceName + ": " + results[foo].SERVICE_valor];}
+
+                                    stepService.set(stepName, array);
+                                }
+                                else {
+                                    if(array.includes(serviceName)==false){
+                                        if(results[foo].SERVICE_valor==null){array.push(serviceName);}
+                                        else{array.push(serviceName + ": " + results[foo].SERVICE_valor);}
+
+                                    }
+                                }
+                                count++;
+                                if (count > total - 1) done();
+                            });
+                        });
+                    });
+                }(i))
+            };
+            function done() {
+                //console.log(serviceOption.getTree());
+                res.render('orderInfo', { title: 'Informação Encomenda', stepService: stepService, steps: steps, services: services, serviceOption: serviceOption});
+            }
+
+
+
+        });
+    }
+});
+
+
+
+module.exports = router;
